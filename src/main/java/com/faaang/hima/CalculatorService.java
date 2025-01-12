5a987bc201eac01484537c944b6bfa0fd6587273
Hima302003 <hima302003@gmail.com>
2025-01-12 16:53:53 +0530

    Initial commit

---
+++ src/main/java/com/faaang/hima/CalculatorService.java
@@ -0,0 +1,8 @@
+package com.faaang.hima;
+
+public interface CalculatorService {
+    int add(int a, int b);
+    int subtract(int a, int b);
+    int multiply(int a, int b);
+    int divide(int a, int b);
+}
